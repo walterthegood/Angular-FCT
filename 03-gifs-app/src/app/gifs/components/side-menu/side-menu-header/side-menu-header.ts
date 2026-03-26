a588af61e0a77434dde38f9fd06e18627dbf875b
walterthegood <waltersalmon2000@gmail.com>
2026-03-26 15:52:19 +0000

    Componentes del menu lateral

---
+++ 03-gifs-app/src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
@@ -0,0 +1,8 @@
+import {  Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-side-menu-header',
+  imports: [],
+  templateUrl: './side-menu-header.html',
+})
+export class SideMenuHeader { }
---
+++ 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending-page',
+  imports: [],
+  templateUrl: './trending-page.html',
+})
+export default class TrendingPage { }
